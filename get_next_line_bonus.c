20a67bbec9ad2ca9243cf346fa665811e998e097
Spacecureuil <p.maury.skj@gmail.com>
2021-06-01 11:02:49 +0200

    norme_bonus

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,7 +6,7 @@
 /*   By: pmaury <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/01 10:45:22 by pmaury            #+#    #+#             */
-/*   Updated: 2021/06/01 10:55:12 by pmaury           ###   ########.fr       */
+/*   Updated: 2021/06/01 11:02:02 by pmaury           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,25 +19,22 @@ int	get_next_line(int fd, char **line)
 	int			i;
 	int			ret;
 
-	if (fd < 0 || fd >= 64 || BUFFER_SIZE < 1 || read(fd, buff[fd], 0) < 0 || !line)
+	if (fd < 0 || fd >= 64 || BUFFER_SIZE < 1 || read(fd, buff[fd], 0) < 0 
+		|| !line)
 		return (-1);
 	*line = NULL;
 	while (42)
 	{
 		i = ft_strnlen(buff[fd]);
+		*line = ft_strnjoin(*line, buff[fd], i);
+		ft_realign(buff[fd], i + 1);
 		if (i == BUFFER_SIZE || buff[fd][i] != '\n')
 		{
-			*line = ft_strnjoin(*line, buff[fd], i);
-			ft_realign(buff[fd], i + 1);
 			ret = read(fd, buff[fd], BUFFER_SIZE);
 			if (ret < 1)
 				return (ret);
 		}
 		else
-		{
-			*line = ft_strnjoin(*line, buff[fd], i);
-			ft_realign(buff[fd], i + 1);
 			return (1);
-		}
 	}
 }
